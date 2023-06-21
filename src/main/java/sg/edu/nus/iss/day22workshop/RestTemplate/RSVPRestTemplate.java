acda53f1968e090a0ab820cbdc74137b4ab7fa21
sunilsingh15 <sunilsingh.1996@hotmail.com>
2023-06-21 15:54:29 +0800

    rest template completed

---
+++ src/main/java/sg/edu/nus/iss/day22workshop/RestTemplate/RSVPRestTemplate.java
@@ -0,0 +1,47 @@
+package sg.edu.nus.iss.day22workshop.RestTemplate;
+
+import java.util.List;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import sg.edu.nus.iss.day22workshop.model.RSVP;
+
+public class RSVPRestTemplate {
+
+    private static final String RSVP_ENDPOINT_URL = "http://localhost:4000/api/rsvps";
+
+    private static RestTemplate restTemplate = new RestTemplate();
+
+    public List<RSVP> getRSVPs() {
+
+        HttpEntity<String> entity = new HttpEntity<String>("parameters");
+
+        ResponseEntity<List<RSVP>> results = restTemplate.exchange(
+                RSVP_ENDPOINT_URL,
+                HttpMethod.GET,
+                entity,
+                new ParameterizedTypeReference<List<RSVP>>() {
+                });
+
+        return results.getBody();
+    }
+
+    public Boolean createRSVP(RSVP newRSVP) {
+        ResponseEntity<Boolean> result = restTemplate.postForEntity(RSVP_ENDPOINT_URL, newRSVP, Boolean.class);
+
+        return result.getBody();
+    }
+
+    public Boolean updateRSVP(RSVP rsvp) {
+        HttpEntity<String> entity = new HttpEntity<String>("parameters");
+
+        ResponseEntity<Boolean> result = restTemplate.exchange(RSVP_ENDPOINT_URL, HttpMethod.PUT, entity, Boolean.class);
+
+        return result.getBody();
+    }
+
+}
